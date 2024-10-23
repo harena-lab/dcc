2ef1df2652613e94f14d21fdfd7a1a9c1e2bee89
André Santanchè <santanche@gmail.com>
2024-10-23 10:59:36 -0300

    feat (cell/skin): new zoom scenario

--- playground/scripts/gallery/cell/mutant-cell-scripts-dev-skin-2-melanin-zoom.js
+++ playground/scripts/gallery/cell/mutant-cell-scripts-dev-skin-2-melanin-zoom.js
@@ -1,4 +1,4 @@
-(function () {
+(() => {
   AuthorCellManager.instance.insertSource(
     'Colors',
     [['empty', '_', { src: 'images/cell/cell-yellow-green.png', width: 25, height: 25, alt: 'empty' }],
@@ -6,12 +6,12 @@
      ['stage-2', '2', { src: 'images/cell/cell-3.png', width: 25, height: 25, alt: 'stage-2' }],
      ['stage-3', '3', { src: 'images/cell/cell-2.png', width: 25, height: 25, alt: 'stage-3' }],
      ['stage-4', '4', { src: 'images/cell/cell-6.png', width: 25, height: 25, alt: 'stage-4' }],
-     ['mut-1',   '5', { src: 'images/cell/cell-1.png', width: 25, height: 25, alt: 'mut-1' }],
-     ['mut-2',   '6', { src: 'images/cell/cell-8.png', width: 25, height: 25, alt: 'mut-2' }],
-     ['mut-3',   '7', { src: 'images/cell/cell-7.png', width: 25, height: 25, alt: 'mut-3' }],
-     ['mut-4',   '8', { src: 'images/cell/cell-5.png', width: 25, height: 25, alt: 'mut-4' }],
+     ['mut-3',   '7', { src: 'images/cell/mela-1.png', width: 25, height: 25, alt: 'mut-3' }],
+     ['mut-1',   '5', { src: 'images/cell/mela-2.png', width: 25, height: 25, alt: 'mut-1' }],
+     ['mut-2',   '6', { src: 'images/cell/mela-3.png', width: 25, height: 25, alt: 'mut-2' }],
+     ['mut-4',   '8', { src: 'images/cell/mela-4.png', width: 25, height: 25, alt: 'mut-4' }],
      ['purple',  'p', { src: 'images/cell/cell-purple.svg', width: 25, height: 25, alt: 'purple' }],
-     ['tile',    't', { src: 'images/cell/cell-tile.svg', width: 25, height: 25, alt: 'tile' }],
+     ['tile',    't', { src: 'images/cell/cell-transparent.svg', width: 25, height: 25, alt: 'tile' }],
      ['yellow',  'y', { src: 'images/cell/cell-yellow.svg', width: 25, height: 25, alt: 'yellow' }],
      ['brown',   'b', { src: 'images/cell/cell-brown.svg', width: 25, height: 25, alt: 'brown' }],
      ['red',     'r', { src: 'images/cell/cell-red.svg', width: 25, height: 25, alt: 'red' }],
@@ -21,20 +21,20 @@
   <block type="transform"></block>
   <block type="single"></block>
 </category>`,
-`<dcc-space-cellular-editor id="cellular-space" rows="28" cols="40"
-  cell-width="20" cell-height="20" cover-image="images/photo/epidermis-dermis.jpg" cover-opacity="100" grid analysis policy="crescent">
+`<dcc-space-cellular-editor id="cellular-space" rows="15" cols="20"
+  cell-width="40" cell-height="40" cover-image="images/photo/epidermis-dermis-zoom.jpg" cover-opacity="100" grid analysis policy="crescent">
 </dcc-space-cellular-editor>
 
 <dcc-cell-image type="1" label="stage-1" image="images/cell/cell-4.png"></dcc-cell-image>
 <dcc-cell-image type="2" label="stage-2" image="images/cell/cell-3.png"></dcc-cell-image>
 <dcc-cell-image type="3" label="stage-3" image="images/cell/cell-2.png"></dcc-cell-image>
 <dcc-cell-image type="4" label="stage-4" image="images/cell/cell-6.png"></dcc-cell-image>
-<dcc-cell-image type="5" label="mut-1" image="images/cell/cell-1.png"></dcc-cell-image>
-<dcc-cell-image type="6" label="mut-2" image="images/cell/cell-8.png"></dcc-cell-image>
-<dcc-cell-image type="7" label="mut-3" image="images/cell/cell-7.png"></dcc-cell-image>
-<dcc-cell-image type="8" label="mut-4" image="images/cell/cell-5.png"></dcc-cell-image>
+<dcc-cell-image type="7" label="mut-3" image="images/cell/mela-1.png"></dcc-cell-image>
+<dcc-cell-image type="5" label="mut-1" image="images/cell/mela-2.png"></dcc-cell-image>
+<dcc-cell-image type="6" label="mut-2" image="images/cell/mela-3.png"></dcc-cell-image>
+<dcc-cell-image type="8" label="mut-4" image="images/cell/mela-4.png"></dcc-cell-image>
 <dcc-cell-image type="p" label="purple" image="images/cell/cell-purple.svg"></dcc-cell-image>
-<dcc-cell-image type="t" label="tile" image="images/cell/cell-tile.svg"></dcc-cell-image>
+<dcc-cell-image type="t" label="tile" image="images/cell/cell-transparent.svg"></dcc-cell-image>
 <dcc-cell-image type="y" label="yellow" image="images/cell/cell-yellow.svg"></dcc-cell-image>
 <dcc-cell-image type="b" label="brown" image="images/cell/cell-brown.svg"></dcc-cell-image>
 <dcc-cell-image type="r" label="red" image="images/cell/cell-red.svg"></dcc-cell-image>
@@ -105,23 +105,23 @@
       </dcc-button>
    </div>
    <div style="flex:10%; max-width:48px; max-height:48px; margin-right:10px">
+      <dcc-button label="Pink" topic="type/mut-3"
+                   image="images/cell/mela-1.png">
+      </dcc-button>
+   </div>
+   <div style="flex:10%; max-width:48px; max-height:48px; margin-right:10px">
       <dcc-button label="Pink" topic="type/mut-1"
-                   image="images/cell/cell-1.png">
+                   image="images/cell/mela-2.png">
       </dcc-button>
    </div>
    <div style="flex:10%; max-width:48px; max-height:48px; margin-right:10px">
       <dcc-button label="Pink" topic="type/mut-2"
-                   image="images/cell/cell-8.png">
-      </dcc-button>
-   </div>
-   <div style="flex:10%; max-width:48px; max-height:48px; margin-right:10px">
-      <dcc-button label="Pink" topic="type/mut-3"
-                   image="images/cell/cell-7.png">
+                   image="images/cell/mela-3.png">
       </dcc-button>
    </div>
    <div style="flex:10%; max-width:48px; max-height:48px; margin-right:10px">
       <dcc-button label="Pink" topic="type/mut-4"
-                   image="images/cell/cell-5.png">
+                   image="images/cell/mela-4.png">
       </dcc-button>
    </div>
    <div style="flex:10%; max-width:48px; max-height:48px; margin-right:10px">
